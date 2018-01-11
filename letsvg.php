a525b002aed482f707bbd779b30e14c2eade716d
Joaquín <joaquin.bj.97@gmail.com>
2018-01-11 13:14:15 +0100

    Ver 1.1
    Recoge todos los PATHs de todos los xml que sigan la estructura indicada en el readme.md

--- letsvg.php
+++ letsvg.php
@@ -5,12 +5,13 @@
     <title>LETsVG</title>
   </head>
   <body>
+    <h2>LET's'VG</h2>
+    <h4>Documentos transformados: </h4><br>
 <?php
-  /*Programa creado por Joaquin Bello
-    GITHUB: https://github.com/JoaquinBelloJimenez
-  */
-    
-  //Crear la carpeta en la que se alamcenará el nuevo documento
+    /*Programa creado por Joaquin Bello
+      GITHUB: https://github.com/JoaquinBelloJimenez */
+
+  //Crear la carpeta en la que se almacenará el nuevo documento
   if (!file_exists('svg')) {
     mkdir('svg', 0777, true);
   }
@@ -27,8 +28,6 @@
     $exportado = fopen($archivo, "w");
     fwrite($exportado, $re);
 
-    echo "<h4>- $archivo </h4>"; //Archivo sin doble punto
-
    $xml = simplexml_load_file($archivo); //Cargar el xml a usar
 
      //Obtener cada elemento
@@ -39,37 +38,44 @@
      $fill = $xml->path['androidfillColor'];
      $path = $xml->path['androidpathData'];
 
+     //Obtener todos los Path
+     foreach ($xml->path as $npath) {
+       $paths[] = $npath;
+     };
+
      //Array que almacena los datos a usar posteriormente con la fabricación del nuevo documento
      $datos = array(
        'width'  =>  $width,
        'height' =>  $height,
        'vHeight'=>  $vHeight,
        'vWidth' =>  $vWidth,
-       'fill'   =>  $fill,
-       'path'   =>  $path
     );
+    generarSvg($archivo,$datos,$paths);
 
-     echo "$archivo ";
-     print_r($datos);
-     echo "<br>";
+    //Limpiar arrays
+    $paths = array();
+    $datos = array();
 
-    generarSvg($archivo,$datos);
-  }
+  } //--Foreach
  ?>
 
-
 <?php
-function generarSvg($prenombre,$datos){
+function generarSvg($prenombre,$datos,$paths){
   //Obtener el nombre del documento a exportar
   $nombre = "svg/".substr($prenombre,0,strlen($prenombre)-4).".svg";
 
   $exportado = fopen("$nombre", "w");
-  $svg = "<svg height='$datos[height]' viewBox='0 0 $datos[vWidth] $datos[vHeight]' width='$datos[width]'> <path fill='$datos[fill]' d='$datos[path]'/></svg>";
+  $svg = "<svg height='$datos[height]' viewBox='0 0 $datos[vWidth] $datos[vHeight]' width='$datos[width]'>";
+  //Insertar cada PATH dentro del documento antes de exportarlo
+  foreach ($paths as $path) {
+    $svg .= "<path fill='$path[androidfillColor]' d='$path[androidpathData]'/>";
+  }
+  $svg .= "</svg>";
 
   //Exportar svg
   fwrite($exportado, $svg);
-  }
-
+  echo "".substr($prenombre,0,strlen($prenombre)-4) ."<br>"; //Nombre de documentos exportados
+  };
 ?>
 </body>
 </html>
